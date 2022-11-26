787e5d31d1ef951a0abf8b7d836e0d4f3fad588e
n0vay <koshantanu@gmail.com>
2022-11-25 22:57:32 -0500

    init

---
+++ modules.js
@@ -0,0 +1,7 @@
+const {people, age} = require('./people.js');
+
+console.log(people, age);
+
+const os = require('os');
+
+console.log(os)
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ["yoshi", "ryu", "owen", "wilson"];
+const age = ["20", "21", "18", "36"];
+
+
+module.exports = {
+    people, age
+};
