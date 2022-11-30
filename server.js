270437f89658bbd98fde43d4ff937f9bc1420675
n0vay <koshantanu@gmail.com>
2022-11-29 20:43:35 -0500

    added html

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    console.log('request made');
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  //set header type
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<head><link rel = "stylesheet" href = "#"></head>');
+  res.write('<p>hello, ninjas</p>');
+  res.write('<p>hello again, ninjas</p>');
+  res.end();
 });
 
-server.listen(3000, 'localhost', ()=>{
-    console.log('we are here for requests on poort 3000')
-})
+server.listen(3000, "localhost", () => {
+  console.log("we are here for requests on poort 3000");
+});
