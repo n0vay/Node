c9368b653799e28f2633ceb5e04588fa2337dd36
n0vay <koshantanu@gmail.com>
2022-11-29 21:01:30 -0500

    setup index page

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   //set header type
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<head><link rel = "stylesheet" href = "#"></head>');
-  res.write('<p>hello, ninjas</p>');
-  res.write('<p>hello again, ninjas</p>');
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+
+  //send html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if(err) {
+        console.log(err);
+        res.end();
+    } else {
+        //res.write(data);
+        res.end(data);
+    }
+  })
 });
 
 server.listen(3000, "localhost", () => {
