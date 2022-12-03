9806a20a913a8474a986c05959ad079b3e15b3b9
n0vay <koshantanu@gmail.com>
2022-12-03 14:43:36 -0500

    added redirection

--- server.js
+++ server.js
@@ -7,32 +7,37 @@ const server = http.createServer((req, res) => {
   //set header type
   res.setHeader("Content-Type", "text/html");
 
-  let path = './views/';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
       res.statusCode = 200;
       break;
-    case '/about':
-      path+= 'about.html';
+    case "/about":
+      path += "about.html";
       res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "./about");
+      res.end();
       break;
     default:
-      path+= '404.html';
+      path += "404.html";
       res.statusCode = 404;
       break;
   }
 
   //send html file
   fs.readFile(path, (err, data) => {
-    if(err) {
-        console.log(err);
-        res.end();
+    if (err) {
+      console.log(err);
+      res.end();
     } else {
-        //res.write(data);
-        res.end(data);
+      //res.write(data);
+      res.end(data);
     }
-  })
+  });
 });
 
 server.listen(3000, "localhost", () => {
